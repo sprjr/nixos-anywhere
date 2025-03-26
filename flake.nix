e1c8061432ea186f517ea2c573ffe3ccc4a1d63f
sprjr <patrick@rawlinson.ws>
2025-03-26 09:54:43 -0600

    removing facter

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixos-facter-module.url = "github:numtide/nixos-facter-modules";
 
   outputs =
     {
